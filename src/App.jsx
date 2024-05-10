621a8ab22557f8b783641dbade9405ccef58735c
Kenwren Apilado <155591015+Kenwren@users.noreply.github.com>
2024-05-10 13:53:28 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/styles.css'
+import './css/reset.css'
+import StarRating from './components/StarRating';
+
+function App() {
+  return (
+    <div>
+      <StarRating />
+    </div>
+  );
+}
+
+export default App;
